df7a1c9ffa438da21770376461b27e91c128cae1
Liam Gilmore <liamgilmore1@gmail.com>
2023-03-25 15:14:41 -0400

    add info to products

--- client/src/Products.js
+++ client/src/Products.js
@@ -2,7 +2,34 @@
 const Products = () => {
 
     return (
-        <div>Products</div>
+        <>
+        <div>
+            <h2>Products</h2>
+            <h3>Natural Ingredients:</h3>
+            <ul>
+                <li>Avocado Oil</li>
+                <li>Grape seed</li>
+                <li>Jojoba</li>
+                <li>Essential Oils</li>
+            </ul>
+            <h4>Nut Free</h4>
+            </div>
+                <div>
+                <h3>Directions for Use:</h3>
+                <ol>
+                    <li>
+                        <ul>
+                            <li>Shake 3-5 drops into your hand</li>
+                            <li>For longer beards, use 7-10 drops</li>
+                        </ul>
+                    </li>
+                    <li>Work the oil into your skin and beard</li>
+                    <li>Use a Beard Comb and/or Boar's Hair Brush to evenly distribute the oil</li>
+                    
+                </ol>
+                <p>Nut Free</p>
+            </div>
+        </>
     );
 };
 
